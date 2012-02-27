6c2ff9f2cc67d3cbc5ca4262a5a84996f3746d0e
Alex Young <alex@bytemark.co.uk>
2012-02-27 23:17:17 +0000

    Refactored Betfair::API to separate SOAP out and make it easier to read

--- lib/betfair/api.rb
+++ lib/betfair/api.rb
@@ -2,131 +2,254 @@ module Betfair
 
   class API
 
+
+    ## Some handy constants...
+
+    EXCHANGE_IDS = {
+      :aus => 2,
+      :uk  => 1
+    }
+    
+    PRODUCT_ID_FREE = 82
+
+    BET_TYPE_LAY  = 'L'
+    BET_TYPE_BACK = 'B'
+
+
+    ## API METHODS
+    #
+
     def place_bet(session_token, exchange_id, market_id, runner_id, bet_type, price, size)		
-      bf_bet = { :marketId => market_id, :selectionId => runner_id, :betType => bet_type, :price => price, :size => size, :asianLineId => 0, :betCategoryType => 'E', :betPersistenceType => 'NONE', :bspLiability => 0 }      
-      response = exchange(exchange_id).request :bf, :placeBets do
-        soap.body = { 'bf:request' => { :header => api_request_header(session_token), :bets => { 'PlaceBets' => [bf_bet] } } }
-      end      
-      error_code = response.to_hash[:place_bets_response][:result][:error_code]
-      error_code2 = response.to_hash[:place_bets_response][:result][:header][:error_code]
-      return error_code == 'OK' ? response.to_hash[:place_bets_response][:result][:bet_results][:place_bets_result] : "#{error_code} - #{error_code2}"  		
+      bf_bet = { 
+        :marketId           => market_id, 
+        :selectionId        => runner_id, 
+        :betType            => bet_type, 
+        :price              => price, 
+        :size               => size, 
+        :asianLineId        => 0, 
+        :betCategoryType    => 'E', 
+        :betPersistenceType => 'NONE', 
+        :bspLiability       => 0 
+      }
+
+      response = exchange(exchange_id).
+        session_request( session_token,
+                         :placeBets, 
+                         :place_bets_response,
+                         :bets => { 'PlaceBets' => [bf_bet] } )
+
+      return response.maybe_result( :bet_results, :place_bets_result )
     end
+
 
     def cancel_bet(session_token, exchange_id, bet_id)
-      response = exchange(exchange_id).request :bf, :cancelBets do
-        soap.body = { 'bf:request' => { :header => api_request_header(session_token), :bets => { 'CancelBets' => [ { :betId => bet_id } ] } } } # "CancelBets" has to be a string, not a symbol!
-      end		
-      error_code = response.to_hash[:cancel_bets_response][:result][:error_code]
-      error_code2 = response.to_hash[:cancel_bets_response][:result][:header][:error_code]
-      return error_code == 'OK' ? response.to_hash[:cancel_bets_response][:result][:bet_results][:cancel_bets_result] : "#{error_code} - #{error_code2}"
+      bf_bet = { :betId => bet_id }
+
+      response = exchange(exchange_id).
+        session_request( session_token,
+                         :cancelBets, 
+                         :cancel_bets_response,
+                         :bets => { 'CancelBets' => [bf_bet] } ) # "CancelBets" has to be a string, not a symbol!
+      
+      return response.maybe_result( :bet_results, :cancel_bets_result )
     end
+
 
     def get_market(session_token, exchange_id, market_id, locale = nil) 
-      response = exchange(exchange_id).request :bf, :getMarket do
-        soap.body = { 'bf:request' => { :header => api_request_header(session_token), :marketId => market_id, :locale => locale } }
-      end
-      error_code = response.to_hash[:get_market_response][:result][:error_code]
-      error_code2 = response.to_hash[:get_market_response][:result][:header][:error_code]
-      return error_code == 'OK' ? response.to_hash[:get_market_response][:result][:market] : "#{error_code} - #{error_code2}"
+      response = exchange(exchange_id).
+        session_request( session_token, 
+                         :getMarket, 
+                         :get_market_response,
+                         :marketId => market_id, 
+                         :locale   => locale )
+
+      return response.maybe_result( :market )
     end
+
 
     def get_market_prices_compressed(session_token, exchange_id, market_id, currency_code = nil)
-      response = exchange(exchange_id).request :bf, :getMarketPricesCompressed do
-        soap.body = { 'bf:request' => { :header => api_request_header(session_token),  :marketId => market_id, :currencyCode => currency_code } }
-      end
-      error_code = response.to_hash[:get_market_prices_compressed_response][:result][:error_code]      
-      error_code2 = response.to_hash[:get_market_prices_compressed_response][:result][:header][:error_code]
-      return error_code == 'OK' ? response.to_hash[:get_market_prices_compressed_response][:result][:market_prices] : "#{error_code} - #{error_code2}"
+      response = exchange(exchange_id).
+        session_request( session_token,
+                         :getMarketPricesCompressed, 
+                         :get_market_prices_compressed_response,
+                         :marketId => market_id,
+                         :currencyCode => currency_code )
+      
+      return response.maybe_result( :market_prices )
     end
+
 
     def get_active_event_types(session_token, locale = nil)
-      response = @global_service.request :bf, :getActiveEventTypes do
-        soap.body = { 'bf:request' => { :header => api_request_header(session_token), 
-            :locale => locale
-          } 
-        }
-      end            
-      error_code = response.to_hash[:get_active_event_types_response][:result][:error_code] 
-      error_code2 = response.to_hash[:get_active_event_types_response][:result][:header][:error_code]      
-      return error_code == 'OK' ? response.to_hash[:get_active_event_types_response][:result][:event_type_items][:event_type] : "#{error_code} - #{error_code2}"      
+      response = @global_service.
+        session_request( session_token,
+                         :getActiveEventTypes, 
+                         :get_active_event_types_response,
+                         :locale => locale )
+
+      return response.maybe_result( :event_type_items, :event_type )
     end
 
+
     def get_all_markets(session_token, exchange_id, event_type_ids = nil, locale = nil, countries = nil, from_date = nil, to_date = nil)
-      response = exchange(exchange_id).request :bf, :getAllMarkets do
-        soap.body = { 'bf:request' => { :header => api_request_header(session_token), 
-            :eventTypeIds => { 'int' => event_type_ids }, 
-            :locale => locale, :countries => { 'country' => countries }, 
-            :fromDate => from_date, 
-            :toDate => to_date 
-          } 
-        }
-      end            
-      error_code = response.to_hash[:get_all_markets_response][:result][:error_code] 
-      error_code2 = response.to_hash[:get_all_markets_response][:result][:header][:error_code]      
-      return error_code == 'OK' ? response.to_hash[:get_all_markets_response][:result][:market_data] : "#{error_code} - #{error_code2}"      
+      response = exchange(exchange_id).
+        session_request( session_token, 
+                         :getAllMarkets, 
+                         :get_all_markets_response,
+                         :eventTypeIds => { 'int' => event_type_ids }, 
+                         :locale       => locale, 
+                         :countries    => { 'country' => countries }, 
+                         :fromDate     => from_date, 
+                         :toDate       => to_date )
+      
+      return response.maybe_result( :market_data )
     end
 
 
     def get_account_funds( session_token, exchange_id )
-      response = exchange( exchange_id ).request :bf, :getAccountFunds do
-        soap.body = { 'bf:request' => { :header => api_request_header(session_token) } }
-      end
-      error_code = response.to_hash[:get_account_funds_response][:result][:error_code]
-      error_code2 = response.to_hash[:get_account_funds_response][:result][:header][:error_code]
-      return error_code == 'OK' ? response.to_hash[:get_account_funds_response][:result] : "#{error_code} - #{error_code2}"
+      response = exchange( exchange_id ).
+        session_request( session_token, 
+                         :getAccountFunds, 
+                         :get_account_funds_response )
+
+      return response.maybe_result
     end
+
 
     def login(username, password, product_id, vendor_software_id, location_id, ip_address)
-      response = @global_service.request :bf, :login do 
-        soap.body = { 'bf:request' => { :username => username, 
-          :password => password, 
-          :productId => product_id, 
-          :vendorSoftwareId => vendor_software_id, 
-          :locationId => location_id, 
-          :ipAddress => ip_address 
-        } 
-        }
-      end      
-      session_token(response.to_hash[:login_response][:result][:header])       
+      response = @global_service.request( :login, 
+                                          :login_response, 
+                                          :username         => username, 
+                                          :password         => password, 
+                                          :productId        => product_id, 
+                                          :vendorSoftwareId => vendor_software_id, 
+                                          :locationId       => location_id, 
+                                          :ipAddress        => ip_address )
+      
+      session_token(response[:header])
     end
+
+    #
+    ## END OF API METHODS
+
 
     def exchange(exchange_id)   
-      exchange_id == 2  ? @aus_service : @uk_service
-    end
-
-    def api_request_header(session_token)      
-      { :client_stamp => 0, :session_token => session_token }
+      exchange_id == EXCHANGE_IDS[:aus] ? @aus_service : @uk_service
     end
 
     def session_token(response_header)      
       response_header[:error_code] == 'OK' ? response_header[:session_token] : response_header[:error_code]
     end
 
+
     def initialize(proxy = nil, logging = nil)
 
-      logging == true ? Savon.log = true : Savon.log = false
+      SOAPClient.log = logging
 
-      @global_service = Savon::Client.new do |wsdl, http|
-        wsdl.endpoint = 'https://api.betfair.com/global/v3/BFGlobalService'
-        wsdl.namespace = 'https://www.betfair.com/global/v3/BFGlobalService'		     
-        http.proxy = proxy if !proxy.nil?
-      end
-
-      @uk_service = Savon::Client.new do |wsdl, http|
-        wsdl.endpoint = 'https://api.betfair.com/exchange/v5/BFExchangeService'
-        wsdl.namespace = 'http://www.betfair.com/exchange/v3/BFExchangeService/UK'
-        http.proxy = proxy if !proxy.nil?
-      end
-
-      @aus_service = Savon::Client.new do |wsdl, http|
-        wsdl.endpoint = 'https://api-au.betfair.com/exchange/v5/BFExchangeService'
-        wsdl.namespace = 'http://www.betfair.com/exchange/v3/BFExchangeService/AUS'
-        http.proxy = proxy if !proxy.nil?
-      end
+      @global_service = SOAPClient.global( proxy )
+      @uk_service     = SOAPClient.uk( proxy )
+      @aus_service    = SOAPClient.aus( proxy )
 
     end
 
-  end
+
+
+
+    # A wrapper around the raw Savon::Client to hide the details of
+    # the Savon API and those parts of the Betfair API which are
+    # constant across the different API method calls
+    class SOAPClient
+
+      # Handy constants
+      NAMESPACES = {
+        :aus    => 'http://www.betfair.com/exchange/v3/BFExchangeService/AUS',
+        :global => 'https://www.betfair.com/global/v3/BFGlobalService',
+        :uk     => 'http://www.betfair.com/exchange/v3/BFExchangeService/UK' }
+      ENDPOINTS  = {
+        :aus    => 'https://api-au.betfair.com/exchange/v5/BFExchangeService',
+        :global => 'https://api.betfair.com/global/v3/BFGlobalService',
+        :uk     => 'https://api.betfair.com/exchange/v5/BFExchangeService' }
+
+
+      # Factory methods for building clients to the different endpoints
+      def self.global( proxy ); new( :global, proxy ); end
+      def self.uk( proxy );     new( :uk, proxy );     end
+      def self.aus( proxy );    new( :aus, proxy );    end
+
+
+      # Wrapper to avoid leaking Savon's logging API
+      def self.log=(logging); Savon.log = !!logging; end
+
+
+      # Pass the `region` (see ENDPOINTS for valid values) to pick the
+      # WSDL endpoint and namespace.  `proxy` should be a string URL
+      # for HTTPI to use as a proxy setting.
+      def initialize( region, proxy )
+        @client = Savon::Client.new do |wsdl, http|
+          wsdl.endpoint  = ENDPOINTS[region]
+          wsdl.namespace = NAMESPACES[region]
+          http.proxy = proxy if proxy
+        end
+      end
+
+
+      # Delegate the SOAP call to bf:`method` with `body` as the
+      # `bf:request` field.  Getting a Hash back, this method returns
+      # response[result_field][:result] as its result.
+      def request( method, result_field, body )
+        response = @client.request( :bf, method ) {
+          soap.body = { 'bf:request' => body }
+        }.to_hash[result_field][:result]
+
+        response.extend( ErrorPresenter )
+        
+        response
+      end
+
+
+      # For those requests which take place in the context of a session,
+      # this method constructs the correct header and delegates to #request.
+      def session_request( session_token, method, result_field, body = {})
+        header_body = { :header => api_request_header(session_token) }
+        full_body = header_body.merge( body )
+
+        request method, result_field, full_body
+      end
+
+
+      def api_request_header(session_token)      
+        { :client_stamp => 0, :session_token => session_token }
+      end
+      protected :api_request_header
+
+
+    end # class SoapClient
+
+
+    # Mix this into a Hash to give it basic error reporting and a nice
+    # path-based data extractor.
+    module ErrorPresenter
+
+      def success?
+        self[:error_code] == "OK"
+      end
+
+
+      def format_error
+        "#{self[:error_code]} - #{self[:header][:error_code]}"
+      end
+
+
+      def maybe_result( *path )
+        success? ? path.inject(self){|m,r| m[r]} : format_error()
+      end
+
+      
+    end # module ErrorPresenter
+
+
+  end # class API
+
+
 
   class Helpers  	  	
 
